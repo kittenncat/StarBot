a3804e6a846a918166f0e31b43ed1d74ad30f414
猫猫 <163902402+kittenncat@users.noreply.github.com>
2025-02-19 00:54:41 +0800

    Update LiveReportGenerator.py

--- starbot/painter/LiveReportGenerator.py
+++ starbot/painter/LiveReportGenerator.py
@@ -404,7 +404,9 @@ class LiveReportGenerator:
         # 底部版权信息，请务必保留此处
         pic.set_row_space(10)
         pic.draw_text_right(50, "Designed By StarBot", Color.GRAY, logo_limit)
+        pic.draw_text_right(50, "本项目为开源项目禁止倒卖", Color.PINK)
         pic.draw_text_right(50, "https://github.com/Starlwr/StarBot", Color.LINK, logo_limit)
+        pic.draw_text_right(50, "本机器人由猫猫进行维护", Color.DEEPSKYBLUE)
         pic.crop_and_paste_bottom()
 
         return pic.base64()
